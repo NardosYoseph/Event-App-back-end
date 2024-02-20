cdc46b2c40324b193fb7965dcc12c006e41f5135
NardosYosef <nardosyosef123@gmail.com>
2024-02-20 08:18:51 +0300

    event fetch

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const uri = 'mongodb+srv://nardos:nardi123@event.bb6br8p.mongodb.net/'; // Change 'myDatabase' to your database name
 
 // Connect to MongoDB
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(uri)
   .then(() => console.log('Connected to MongoDB'))
   .catch(error => console.error('Error connecting to MongoDB:', error));
  // client.close();
