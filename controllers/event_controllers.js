6a7ea38726396709566f9c9dc66afb2e45fb2ae5
NardosYosef <nardosyosef123@gmail.com>
2024-02-20 08:15:40 +0300

    event fetch

--- controllers/event_controllers.js
+++ controllers/event_controllers.js
@@ -43,7 +43,7 @@ async function fetchEvent(req, res) {
       const imagePath = path.join('public', event._doc.image);
       if (fs.existsSync(imagePath)) {
         const imageData = await fs.promises.readFile(imagePath); // Read image file
-      const base64Image = Buffer.from(imageData).toString('base64'); // Convert image data to base64
+     // const base64Image = Buffer.from(imageData).toString('base64'); // Convert image data to base64
       return {
         _id: event._doc._id,
         description: event._doc.description,
@@ -51,7 +51,7 @@ async function fetchEvent(req, res) {
         time: event._doc.time,
         rate: event._doc.rate,
         people: event._doc.people,
-        image: base64Image,
+        image: imageData,
       }; } else {
         // Handle case where the file does not exist
         console.error(`File not found: ${imagePath}`);
