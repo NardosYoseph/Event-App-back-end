31693519a72b20fd80999edf1d0a9040043e6a77
NardosYosef <nardosyosef123@gmail.com>
2024-03-05 23:19:58 +0300

    fix

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   username:{type: String ,unique:false, required: true},
-  profilePicture:{type: String ,unique:false, required: true},
+  profilePicture:{type: String ,unique:false, required: false},
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
   tickets: {type: Number,required:false }
