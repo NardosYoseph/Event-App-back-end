bdcbfd151344dc19a116d2bfbd1511c095f0d673
NardosYosef <nardosyosef123@gmail.com>
2024-03-15 11:47:13 +0300

    fix

--- routes/event_route.js
+++ routes/event_route.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const passport= require("../middleware/checkUser")
+const passport= require("../config/passport")
 
 const eventController = require('../controllers/event_controllers');
 
-router.post('/create',passport,eventController.createEvent);
+router.post('/create',passport.authenticate('jwt'),eventController.createEvent);
 router.get('/allEvents', eventController.fetchEvent);
 router.post('/event', eventController.fetchEventbyID);
 
