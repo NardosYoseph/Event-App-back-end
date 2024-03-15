4ff98fab362831372aac3f21d0e82615a8def7ce
NardosYosef <nardosyosef123@gmail.com>
2024-03-15 18:31:29 +0300

    fix

--- routes/user_route.js
+++ routes/user_route.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user_Controller');
+const passport= require("../middleware/passport")
+
 
 router.post('/register', userController.register);
 
