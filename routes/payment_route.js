36c0be07f32397da9997b1429d206d0aed1e5314
NardosYosef <nardosyosef123@gmail.com>
2024-03-15 18:35:42 +0300

    fix

--- routes/payment_route.js
+++ routes/payment_route.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 const paymentController = require('../controllers/payment_controller');
 
-router.post('/paymentStatus',paymentController.verifyPayment);
+router.post('/paymentStatus',paymentController.paymentStatus);
 
 module.exports=router;
