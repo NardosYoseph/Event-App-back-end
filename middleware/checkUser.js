7b195db0dc5a5f8cd9c441fb3ddac8c54b54a0cd
NardosYosef <nardosyosef123@gmail.com>
2024-03-15 11:26:55 +0300

    fix

--- middleware/checkUser.js
+++ middleware/checkUser.js
@@ -3,6 +3,6 @@ const passport =require("../config/passport")
 passport.authenticate('jwt', { session: false }, async (err, user, info) => {
     if (err || !user) {
       return res.status(401).json({ error: 'Unauthorized' });
-    }})(req,res);
+    }})
     
 module.exports = {passport}
