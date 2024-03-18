2772eb7c3cd729329e968e5df52e67b560f32cc2
NardosYosef <nardosyosef123@gmail.com>
2024-03-18 15:38:44 +0300

    fix

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use('/api/user', userRoutes);
 app.use('/api/event', eventRoutes);
 app.use('/api/payment', paymentRoutes);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
--- controllers/payment_controller.js
+++ controllers/payment_controller.js
@@ -2,8 +2,12 @@ const eventController = require('../controllers/event_controllers');
 
 async function paymentStatus(req, res) {
     console.log('Received callback:', req);
-        res.status(200);
+    const paymentStatus = { // Simulate payment status object
+        success: true,
+        message: 'Payment finished!'
+      };
     
+      res.status(200).json(paymentStatus);
   }
 
   module.exports = {paymentStatus};
