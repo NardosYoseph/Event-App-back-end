38d18482076ebfa276c74cc58fa7156deb36a1e0
NardosYosef <nardosyosef123@gmail.com>
2024-03-20 18:47:18 +0300

    fix

--- controllers/payment_controller.js
+++ controllers/payment_controller.js
@@ -8,15 +8,13 @@ async function paymentStatus(req, res) {
     console.log('Received callback:', req.body);
    const txnRef=req.params.txnRef;
  const response =await verifyPayment(txnRef);
-if(response["status"]=="successful"){
-  eventController.buyTicket
-}
-      res.status(200).json(paymentStatus);
+
+      res.status(200).json(response);
   }
 
   async function verifyPayment(txnRef) {
     try{
-    const payment_status= axios.get(`https://api.chapa.co/v1/transaction/verify/${txnRef}`);
+    const payment_status= await axios.get(`https://api.chapa.co/v1/transaction/verify/${txnRef}`);
     console.log("verification",payment_status)
     }catch{
     }
