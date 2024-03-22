369e617d626b1537a28c4908cb27a94d14812151
NardosYosef <nardosyosef123@gmail.com>
2024-03-22 11:46:48 +0300

    fix

--- controllers/payment_controller.js
+++ controllers/payment_controller.js
@@ -8,7 +8,7 @@ async function paymentStatus(req, res) {
   console.log('Received callback:', req.body);
   console.log('Received callback:', req.params);
 
-  const tx_ref = req.params.trx_ref;
+  const tx_ref = req.params.tnxRef;
   const eventId=req.params.eventId;
   const userId=req.params.userId;
 
--- services/payment_service.js
+++ services/payment_service.js
@@ -1,27 +1,11 @@
 const dbConnection = require('../config/database'); 
 const Payment = require('../models/payment');
 
-async function storePayment(paymentData){
-dbConnection;
-const payment=new Payment(paymentData);
-await Payment.save;
-return payment;
-
-}
-async function retrieveUserEventId(txnRef){
-    dbConnection;
-    const payment = await Payment.findOne({ txnRef }).populate('userId eventId');
-      
-          if (!payment) {
-            console.log("ft not found in db")
-            throw new Error('Payment not found');
-    }
-    return { userId: payment.userId, eventId: payment.eventId };
-}
-    
+async function storePayment(paymentData) {
+    const payment = new Payment(paymentData);
+    await payment.save();
+    return payment;
+  }
 
 
-module.exports={
-    storePayment,
- retrieveUserEventId,
-}
+module.exports={storePayment}
