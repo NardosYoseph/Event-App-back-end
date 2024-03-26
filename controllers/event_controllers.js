3d229805953d65a0c2c5993d577ac1f7979042bb
NardosYosef <nardosyosef123@gmail.com>
2024-03-26 11:07:54 +0300

    fix

--- controllers/event_controllers.js
+++ controllers/event_controllers.js
@@ -29,7 +29,7 @@ async function fetchEvent(req, res) {
           price: event._doc.price,
           availableTickets:event._doc.availableTickets,
           eventorganizer:event._doc.eventorganizer,
-         atendees:event._doc.atendees,
+         attendees:event._doc.attendees,
          status:event._doc.status
 
         }; 
